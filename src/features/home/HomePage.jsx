17b6264ff84f31b817adcda8213724f599422c00
chet <chadseka@gmail.com>
2018-05-26 19:24:34 -0500

    add the end of section 20

--- src/features/home/HomePage.jsx
+++ src/features/home/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = ({ history }) => {
               src="/assets/logo.png"
               alt="logo"
             />
-            <div className="content">Re-vents</div>
+            <div className="content">Event Hunt</div>
           </h1>
           <h2>Do whatever you want to do</h2>
           <div onClick={() => history.push('/events')} className="ui huge white inverted button">
@@ -20,23 +20,7 @@ const HomePage = ({ history }) => {
           </div>
         </div>
       </div>
-      <div style={{ textAlign: 'center' }}>
-        Icons made by{' '}
-        <a href="http://www.freepik.com" title="Freepik">
-          Freepik
-              </a>{' '}
-        from{' '}
-        <a href="https://www.flaticon.com/" title="Flaticon">
-          www.flaticon.com
-              </a>{' '}
-        is licensed by{' '}
-        <a
-          href="http://creativecommons.org/licenses/by/3.0/"
-          title="Creative Commons BY 3.0"
-        >
-          CC 3.0 BY
-              </a>
-      </div>
+
     </div>
   )
 }
